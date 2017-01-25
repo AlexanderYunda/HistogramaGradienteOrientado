24087bd1c0e6dff7f681ac99038b98f69b8533f2
AlexanderYunda <hallowedjhon@hotmail.com>
2017-01-25 16:34:31 -0500

    Update Principal.cpp

--- Principal.cpp
+++ Principal.cpp
@@ -11,55 +11,29 @@
 #define orientaciones 9
 
 
-//Gradientes *mscGrad;// CREO UN PUNTERO A LA CLASE GRADIENTE
-//Celdas *micelda;
+
 HOG *miHOG;
 clock_t start, end1;
 
 
 
 void main(void){
-//mscGrad = new Gradientes(cvSize(ancho_Imagen, alto_Imagen));// CREO EL CONTRUCTOR GRADIENTE
 start=clock();
-//micelda=new Celdas(cvSize(ancho_Imagen, alto_Imagen), ancho_Celda,alto_Celda, orientaciones); 
-	miHOG=new HOG(cvSize(ancho_Imagen, alto_Imagen), ancho_Celda,alto_Celda,ancho_Bloque,alto_Bloque, orientaciones);
+miHOG=new HOG(cvSize(ancho_Imagen, alto_Imagen), ancho_Celda,alto_Celda,ancho_Bloque,alto_Bloque, orientaciones);
 IplImage *img1 = cvLoadImage(DIR_CEDA_PASO,CV_LOAD_IMAGE_GRAYSCALE);//CARGO IMAGEN EN ESCALA DE GRISES
 IplImage *img2 = cvCreateImage(cvSize(ancho_Imagen, alto_Imagen),8,1);//CREO UNA IMAGEN CON MEDIDAS ESPECIFICAS
 ajustarTamano(img1,img2);// AJUSTO EL TAMANO A IMAGENES DE 32X32
 cvEqualizeHist(img2,img2);// ECUALIZO LA IMAGEN (ESCALA DE GRISES)
-//micelda->matrizHistogramaCelda(img2);
-
-
-//MUESTRA LOS HISTOGRAMAS POR CELDA
-/*
-for(int i=0;i<micelda->celda_y;i++)
-	{
-		for(int j=0;j<micelda->celda_x;j++)
-		{
-			for(int k=0;k<micelda->orientacion;k++)
-			{
-			printf("En celda %d,%d el histograma orientado %d = %f \n",i+1,j+1,k+1,micelda->HistCelda[i][j].Hcelda[k]);
-			}
-
-		}
-	}
-
-*/
-
-
-//mscGrad->calcularImgMagAng(img2);
-
-
 miHOG->vectorHOG(img2);
 
-
-//cvShowImage("Ceda el Paso",img1);
-//cvShowImage("Ecualizacion de Ceda el Paso",img2);
+cvShowImage("Ceda el Paso",img1);//Imagen Original
+cvShowImage("Ecualizacion de Ceda el Paso",img2);//Imagen ajustado tamaño y ecualizada
+	
 //MUESTRA LAS IMAGENES DE MAGNITUD Y ANGULO
-//cvShowImage("Angulos",mscGrad->angles);
-//cvShowImage("Magnitud",mscGrad->magnit);
+cvShowImage("Angulos",mscGrad->angles);
+cvShowImage("Magnitud",mscGrad->magnit);
 end1=clock();
-printf("Tiempo= %d ms \n",end1-start);
+printf("Tiempo= %d ms \n",end1-start);// Tiempo de computo del descriptor
 cvWaitKey(0);
 system("Pause");
 
